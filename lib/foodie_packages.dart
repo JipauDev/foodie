d2b9666e6a47b08fa33b21049a8cf9418953187d
JipauDev <51697654+JipauDev@users.noreply.github.com>
2023-08-07 09:21:31 +0700

    Update foodie_packages.dart

--- lib/foodie_packages.dart
+++ lib/foodie_packages.dart
@@ -1,7 +1,14 @@
-library foodie_packages;
+library event;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'modal/modal_event.dart';
+export 'modal/modal_intro.dart';
+export 'modal/modal_select_interest.dart';
+export 'modal/model_country.dart';
+export 'modal/notification_model.dart';
+export 'modal/modal_card.dart';
+export 'modal/modal_feature_event.dart';
+export 'modal/modal_popular_event.dart';
+export 'service/app_service.dart';
+export 'service/helper_notifications.dart';
+export 'service/notification_hive.dart';
+export 'data/data_file.dart';
